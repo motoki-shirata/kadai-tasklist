4d89b49b86b05f2a3c7774a15d24168110908ca7
motoki.shirata <shirata.motoki@lmi.ne.jp>
2020-06-08 09:02:36 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
 
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -17,7 +18,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to '/'
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
